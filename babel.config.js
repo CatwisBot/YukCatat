3428a846134a47380a6c441a76c1c44984ea967a
CatwisBot <whisnuaryanugraha04@gmail.com>
2025-10-23 19:38:50 +0700

    feat: add react-native-reanimated plugin to babel configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  // Required for react-native-reanimated (used by nativewind + animations)
+  plugins: ['react-native-reanimated/plugin'],
 };
